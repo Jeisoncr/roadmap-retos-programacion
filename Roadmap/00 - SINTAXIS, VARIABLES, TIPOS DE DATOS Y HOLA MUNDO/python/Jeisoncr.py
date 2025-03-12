0e5592f0ef40b9772f9a9d9e9f52d4d3ebe95859
Jeison Cruz <91863308+Jeisoncr@users.noreply.github.com>
2025-03-12 13:26:26 -0500

    Create Jeisoncr.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jeisoncr.py
@@ -0,0 +1,13 @@
+#https://python.org
+#""" comentario en python """
+
+mi_variable = "Mi variable"
+
+MI_CONSTANTE = "MI CONSTANTE"
+
+mi_int=1
+mi_float=2.5
+mi_bool=False
+mi_string='mi primer texto'
+
+print('Hola! Python!')
